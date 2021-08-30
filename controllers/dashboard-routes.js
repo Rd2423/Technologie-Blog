82ed6b84638324983a991b519c773f80a48ad6dc
Rayan Diouf <rayandiouf@Rayans-MacBook-Pro.local>
2021-08-29 20:30:51 -0400

    working on routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,64 @@
+const router = require("express").Router();
+const withAuth = require("../utils/auth");
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+
+router.get("/", withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "post_content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "user_id", "post_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((data) => {
+      const post = data.map((post) => post.get({ plain: true }));
+      res(render("dashboard", { post, loggedIn: true }));
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/edit/:id", withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: ["id", "title", "post_content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const post = dbPostData.get({ plain: true });
+
+      res.render("edit-post", { post, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS technologie_blog_db;
+
+CREATE DATABASE technologie_blog_db;
